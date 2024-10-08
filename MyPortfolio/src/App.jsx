62a794231485c125083c3b83103fc9039037ae19
prabinthapa10 <prabinmagar636@gmail.com>
2024-10-08 22:04:41 +0545

    hero section complete

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 export default function App() {
   return (
     <div className={styles.app}>
       <NavBar />
+      <Hero />
     </div>
   );
 }
---
+++ MyPortfolio/src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { getImageUrl } from "../utils";
+import styles from "./Hero.module.css";
+export default function Hero() {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}> Hi, I'm Prabin.</h1>{" "}
+        <p className={styles.desc}>
+          I am full stack developer with no experience. I am a learner. Reach
+          out if you like to learn more!
+        </p>
+        <a
+          className={styles.contactBtn}
+          href="mailto:prabinmagarg636@gmail.com"
+        >
+          Contact me
+        </a>
+      </div>
+      <img
+        className={styles.heroImg}
+        src={getImageUrl("hero/heroImage.png")}
+        alt="image of me"
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bttomBlur} />
+    </section>
+  );
+}
